cc98f6585a6e6988d5159b6ff40af165305df20d
Patrick Sabanic <patrick.sabanic@tum.de>
2024-01-31 12:44:50 +0000

    Fix test code

--- module/test.asm
+++ module/test.asm
@@ -1,17 +1,8 @@
 BITS 64
 section .text
 _start:
+    mov rax, 0x1002000
+    mov rdx, 0xFFFFFFFF
+    mov [rax], rdx
     mov rax,0xa
-_prep:
-    mov rdx,100
-_loop:
-    push rax
-    inc rax
-    dec rdx
-    jnz _loop
-    mov rdx,100
-_clean_loop:
-    pop rcx
-    dec rdx
-    jnz _clean_loop
-    jmp _prep
+    jmp _start
