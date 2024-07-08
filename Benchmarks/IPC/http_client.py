0abf52ecafbc9e3da47337508abc2e72997d2e6a
Teofil98 <teo.bodea12@yahoo.com>
2024-07-08 11:10:18 +0200

    Add HTTPS support

--- Benchmarks/IPC/http_client.py
+++ Benchmarks/IPC/http_client.py
@@ -1,16 +1,26 @@
 import http.client
+import ssl
+import urllib
+import json
 from ctypes import *
 
 try:
+    d= 'a' * 4096
+    #data = urllib.parse.urlencode(d).encode('utf-8')
+    #print(f'Data: {data}')
+    foo = {'text' : f'{d}'}
+    headers = {'Content-Length': '4096'}
+    my_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+    my_context.load_verify_locations('/root/cert.pem')
     my_outb_so = CDLL("/root/my_outb.so")
     my_outb_so.my_outb(245)
-    connection = http.client.HTTPConnection("172.45.0.2:4443")
-    connection.request("GET", "/")
+    connection = http.client.HTTPSConnection("172.45.0.2:4443", context=my_context)
+   # connection.request("GET", "/")
+    connection.request("POST", "/", json.dumps(foo), headers)
     response = connection.getresponse()
-    my_outb_so.my_outb(246)
-    print("Status: {} and reason: {}".format(response.status, response.reason))
+    #print("Status: {} and reason: {}".format(response.status, response.reason))
 
     connection.close()
+    my_outb_so.my_outb(246)
 except Exception as e:
     print("An exception occured: ", e);
-
--- Benchmarks/IPC/http_server.py
+++ Benchmarks/IPC/http_server.py
@@ -10,18 +10,28 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         my_outb_so.my_outb(247)
-        print("Got a request")
+        #print("Got a request")
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b'Hello, world!')
+        #self.wfile.write(b'Hello, world!')
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        #print(f'Content len {content_length}')
+        body = self.rfile.read(content_length)
+        #print(f'Body {body}')
+        my_outb_so.my_outb(247)
+        self.send_response(200)
+        self.end_headers()
+        #self.wfile.write(b'Hello, world!')
+
+    def log_message(self, format, *args):
+        return
 
 
 httpd = HTTPServer(('172.45.0.2', 4443), SimpleHTTPRequestHandler)
 
-#httpd.socket = ssl.wrap_socket (httpd.socket, 
-#                                keyfile="/root/key.pem", 
-#                                certfile='/root/cert.pem', server_side=True)
+httpd.socket = ssl.wrap_socket (httpd.socket,
+                                keyfile="/root/key.pem",
+                                certfile='/root/cert.pem', server_side=True)
 
 httpd.serve_forever()
-
-
