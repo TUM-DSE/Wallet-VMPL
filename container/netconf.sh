f05f422482ac9ea1cb8457ebfa64362aa82a5d71
Patrick Sabanic <patrick.sabanic@tum.de>
2024-11-28 10:22:30 +0000

    Update VM address configuration

--- container/netconf.sh
+++ container/netconf.sh
@@ -2,11 +2,22 @@
 sub=$(expr $(id -u) - 1000)
 YAMLIN=./container/99_config.yaml.in
 YAMLOUT=./container/99_config.yaml
+
+if [ $sub -ge 1000 ]; then
+    sub=$(expr ${sub} - 1000)
+    Addr="10.10.${sub}.10/24"
+    Route="10.10.10.1"
+else
+   Addr="192.168.${sub}.10/24"
+   Route="192.168.${sub}.1"
+
+fi
 cp ${YAMLIN} ${YAMLOUT}
-Addr="192.168.${sub}.10/24"
-Route="192.168.${sub}.1"
 yq -i -y '.network.ethernets.enp0s2.addresses[0] = "'${Addr}'"' ${YAMLOUT} > /dev/null
 yq -i -y '.network.ethernets.enp0s3.addresses[0] = "'${Addr}'"' ${YAMLOUT} > /dev/null
 
 yq -i -y '.network.ethernets.enp0s2.routes[0].via = "'${Route}'"' ${YAMLOUT} > /dev/null
 yq -i -y '.network.ethernets.enp0s3.routes[0].via = "'${Route}'"' ${YAMLOUT} > /dev/null
+
+echo "${Addr}" > container/address
+echo "${Route}" > container/route
