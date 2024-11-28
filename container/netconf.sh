dbc572cf15d392a3caf9f5699bd0e8b9c8074ed5
Patrick Sabanic <patrick.sabanic@tum.de>
2024-11-28 10:30:07 +0000

    Update netconf script

--- container/netconf.sh
+++ container/netconf.sh
@@ -6,7 +6,7 @@ YAMLOUT=./container/99_config.yaml
 if [ $sub -ge 1000 ]; then
     sub=$(expr ${sub} - 1000)
     Addr="10.10.${sub}.10/24"
-    Route="10.10.10.1"
+    Route="10.10.${sub}.1"
 else
    Addr="192.168.${sub}.10/24"
    Route="192.168.${sub}.1"
@@ -19,5 +19,3 @@ yq -i -y '.network.ethernets.enp0s3.addresses[0] = "'${Addr}'"' ${YAMLOUT} > /de
 yq -i -y '.network.ethernets.enp0s2.routes[0].via = "'${Route}'"' ${YAMLOUT} > /dev/null
 yq -i -y '.network.ethernets.enp0s3.routes[0].via = "'${Route}'"' ${YAMLOUT} > /dev/null
 
-echo "${Addr}" > container/address
-echo "${Route}" > container/route
