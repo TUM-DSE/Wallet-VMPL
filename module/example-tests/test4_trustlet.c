8e9fe1d4878d8cd32531343f42fb9c3f31355c36
Pogobanane <aenderboy@gmx.de>
2026-02-08 16:31:45 +0000

    test4: refactor trustlet

--- module/example-tests/test4_trustlet.c
+++ module/example-tests/test4_trustlet.c
@@ -3,11 +3,30 @@
 #include <string.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "cpuid.c"
 
 #define PORT 0xF4
 #define DATA_IN 0x28000000000
 #define DATA_OUT 0x30000000000
+
+void main_default(bool suppress_output) {
+    char* input = (char*)DATA_IN;
+    char* output = (char*)DATA_OUT;
+    trustlet_exit();
+
+    while (1) {
+        strcpy(output, input);
+
+        if (suppress_output) {
+            output[1] += 1;
+            trustlet_exit();
+        } else {
+            output[2] += 1;
+            notify_monitor();
+        }
+    }
+}
 
 int main(int argc, char** argv) {
 
@@ -22,18 +41,10 @@ int main(int argc, char** argv) {
     trustlet_exit();
 
     if(input[0] != 'x'){
-        trustlet_exit();
-        while(1){
-            strcpy(output, input);
-            output[1] += 1;
-            notify_monitor();
-        }
+        bool suppress_output = true;
+        main_default(suppress_output);
     } else {
-        trustlet_exit();
-        while(1){
-            strcpy(output, input);
-            output[2] += 1;
-            notify_monitor();
-        }
+        bool suppress_output = false;
+        main_default(suppress_output);
     }
 }
