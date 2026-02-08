51e7ec56eb40bdf9a7fa2cc96d5463bae66ee54e
Pogobanane <aenderboy@gmx.de>
2026-02-08 16:21:55 +0000

    test4: init from test3

---
+++ module/example-tests/test4_trustlet.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <sys/io.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include "cpuid.c"
+
+#define PORT 0xF4
+#define DATA_IN 0x28000000000
+#define DATA_OUT 0x30000000000
+
+int main(int argc, char** argv) {
+
+    char* input = (char*)DATA_IN;
+    char* output = (char*)DATA_OUT;
+    finalize_zygote();
+    int type = 0;
+    int data_size = 64 * 1024;
+
+    char* buf = malloc(2097152);
+
+    trustlet_exit();
+
+    if(input[0] != 'x'){
+        trustlet_exit();
+        while(1){
+            strcpy(output, input);
+            output[1] += 1;
+            notify_monitor();
+        }
+    } else {
+        trustlet_exit();
+        while(1){
+            strcpy(output, input);
+            output[2] += 1;
+            notify_monitor();
+        }
+    }
+}
