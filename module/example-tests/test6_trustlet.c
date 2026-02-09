3cd7f318207a98a1928d93c1e9e4b75d2ed16114
Pogobanane <aenderboy@gmx.de>
2026-02-09 08:34:12 +0000

    init test6 from test5

---
+++ module/example-tests/test6_trustlet.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <sys/io.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "cpuid.c"
+
+#define PORT 0xF4
+#define DATA_IN 0x28000000000
+#define DATA_OUT 0x30000000000
+
+void main_default(bool suppress_output) {
+    char* input = (char*)DATA_IN;
+    char* output = (char*)DATA_OUT;
+    trustlet_exit();
+
+    while (1) {
+        strcpy(output, input);
+
+        if (suppress_output) {
+            output[1] += 1;
+            trustlet_exit();
+        } else {
+            output[2] += 1;
+            notify_monitor();
+        }
+    }
+}
+
+int main(int argc, char** argv) {
+
+    char* input = (char*)DATA_IN;
+    char* output = (char*)DATA_OUT;
+    finalize_zygote();
+    int type = 0;
+    int data_size = 64 * 1024;
+
+    char* buf = malloc(2097152);
+
+    trustlet_exit();
+
+    if(input[0] != 'x'){
+        bool suppress_output = true;
+        main_default(suppress_output);
+    } else {
+        bool suppress_output = false;
+        main_default(suppress_output);
+    }
+}
