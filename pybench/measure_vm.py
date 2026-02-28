92f6c8f7e13c24ece53e208e7ca44f61e349a3d2
Pogobanane <aenderboy@gmx.de>
2026-02-28 23:12:46 +0000

    measure_vm: dynamic systems

--- pybench/measure_vm.py
+++ pybench/measure_vm.py
@@ -62,12 +62,52 @@ class PktgenTest(AbstractBenchTest):
             # without trustlets, make all will fail (more specifically building the fs)
             server.exec(f"make -C {PROJECT_ROOT}/module/example-dpdk {' '.join(dpdk_examples)} -B CFLAGS=\"{cflags}\"")
 
-    def run(self, server: Server, repetition: int):
-        remote_output_file = "/tmp/output.log"
-        local_output_file = self.output_filepath(repetition)
-        server.exec(f"sudo {PROJECT_ROOT}/{TARGET[self.system]} -l 0,6-12 > {remote_output_file} 2>&1")
-        server.copy_from(remote_output_file, local_output_file)
-        pass
+    def run(self, host: Server, guest: Server, repetition: int):
+        sleep(1) # for good measure
+        remote_mirror_output = "/tmp/mirror_output.log"
+        guest.exec(f"rm {remote_mirror_output} || true")
+        # print("Manually run in guest and wait for 'Core 0 receiving packets': gdb --ex run --args ./module/example-dpdk/mirror -l 0 --no-huge --iova-mode=pa")
+        # print("cd module/example-dpdk; gdb --ex run --args ./noiomgr_run -l 0 --no-huge --iova-mode=pa")
+        # breakpoint()
+        # guest.tmux_new("workload", f"gdb --ex run --args ./module/example-dpdk/mirror -l 0 --no-huge --iova-mode=pa") # | tee {remote_mirror_output}")
+        # guest.tmux_new("workload", f"cd module/example-dpdk; ./noiomgr_run -l 0 --no-huge --iova-mode=pa | tee {remote_mirror_output}")
+        # guest.wait_for_success(f"grep 'Core 0 receiving packets.' {remote_mirror_output}", timeout=30)
+
+        guest.exec("rm /tmp/.dpdk-running || true")
+        if self.system == "mirror":
+            guest.tmux_new("workload", f"./module/example-dpdk/mirror -l 0 --no-huge --iova-mode=pa") # | tee {remote_mirror_output}")
+        elif self.system == "noiomgr":
+            guest.tmux_new("workload", f"cd ./module/example-dpdk; gdb -ex run --args ./noiomgr_run -l 0 --no-huge --iova-mode=pa") # | tee {remote_mirror_output}")
+        else:
+            raise ValueError(f"Unknown system {self.system}")
+
+        # breakpoint()
+        # sleep(30)
+        # guest.wait_for_success(f"grep 'Core 0 receiving packets.' {remote_mirror_output}", timeout=30)
+        guest.wait_for_success(f"test -f /tmp/.dpdk-running", timeout=30)
+
+        # print(host.exec_pktgen('printf("asdfasdfasdf\\n")'))
+        # host.exec_pktgen('prints("portStats", pktgen.portStats("0", "rate"))')
+        # host.exec_pktgen('prints("portStats", pktgen.portStats("0", "port"))')
+        # host.exec_pktgen('prints("pktStats", pktgen.portStats("0", "rate"))')
+        host.exec_pktgen('pktgen.set("all", "size", 64)')
+        host.exec_pktgen('pktgen.start(0)')
+        sleep(3)
+        pps = []
+        for _ in range(5):
+            pps += [ int(host.exec_pktgen('printf(pktgen.portStats("0", "rate")[0].pkts_rx)')) ]
+            sleep(1)
+        host.exec_pktgen('pktgen.stop(0)')
+
+        print(f"Mean Mpps: {np.mean(pps)/1e6:.3f} (stddev: {np.std(pps)/1e6:.3f})")
+
+
+        # remote_output_file = "/tmp/output.log"
+        # local_output_file = self.output_filepath(repetition)
+        # server.exec(f"sudo {PROJECT_ROOT}/{TARGET[self.system]} -l 0,6-12 > {remote_output_file} 2>&1")
+        # server.copy_from(remote_output_file, local_output_file)
+        # pass
+
 
     def parse_results(self, repetition):
         local_output_file = self.output_filepath(repetition)
@@ -110,7 +150,7 @@ def main(measurement: Measurement, plan_only: bool = False) -> None:
             workload = [ 0 ], # , 100 ],
             chaining = [3],
             # system = [ "noiomgr" ],
-            system = [ "mirror" ],
+            system = [ "mirror", "noiomgr" ],
             pktsize = [ 64 ],
 
             # legacy args
@@ -157,38 +197,10 @@ def main(measurement: Measurement, plan_only: bool = False) -> None:
                 # guest.exec(f"dpdk-devbind.py -b vfio-pci {guest.test_iface_addr} --noiommu-mode")
                 remote_dpdk_path = host.exec(f"realpath {PROJECT_ROOT}/.nix-builds/dpdk").strip()
                 guest.exec(f"{remote_dpdk_path}/bin/dpdk-devbind.py -b vfio-pci {guest.test_iface_addr} --noiommu-mode")
-                sleep(1) # for good measure
-                remote_mirror_output = "/tmp/mirror_output.log"
-                guest.exec(f"rm {remote_mirror_output} || true")
-                # print("Manually run in guest and wait for 'Core 0 receiving packets': gdb --ex run --args ./module/example-dpdk/mirror -l 0 --no-huge --iova-mode=pa")
-                # print("cd module/example-dpdk; gdb --ex run --args ./noiomgr_run -l 0 --no-huge --iova-mode=pa")
-                # breakpoint()
-                # guest.tmux_new("workload", f"gdb --ex run --args ./module/example-dpdk/mirror -l 0 --no-huge --iova-mode=pa") # | tee {remote_mirror_output}")
-                # guest.tmux_new("workload", f"cd module/example-dpdk; ./noiomgr_run -l 0 --no-huge --iova-mode=pa | tee {remote_mirror_output}")
-                # guest.wait_for_success(f"grep 'Core 0 receiving packets.' {remote_mirror_output}", timeout=30)
 
-                guest.exec("rm /tmp/.dpdk-running || true")
-                # guest.tmux_new("workload", f"./module/example-dpdk/mirror -l 0 --no-huge --iova-mode=pa") # | tee {remote_mirror_output}")
-                guest.tmux_new("workload", f"cd ./module/example-dpdk; gdb -ex run --args ./noiomgr_run -l 0 --no-huge --iova-mode=pa") # | tee {remote_mirror_output}")
-                # breakpoint()
-                # sleep(30)
-                # guest.wait_for_success(f"grep 'Core 0 receiving packets.' {remote_mirror_output}", timeout=30)
-                guest.wait_for_success(f"test -f /tmp/.dpdk-running", timeout=30)
+                for repetition in range(test.repetitions):
+                    test.run(host, guest, repetition)
 
-                # print(host.exec_pktgen('printf("asdfasdfasdf\\n")'))
-                # host.exec_pktgen('prints("portStats", pktgen.portStats("0", "rate"))')
-                # host.exec_pktgen('prints("portStats", pktgen.portStats("0", "port"))')
-                # host.exec_pktgen('prints("pktStats", pktgen.portStats("0", "rate"))')
-                host.exec_pktgen('pktgen.set("all", "size", 64)')
-                host.exec_pktgen('pktgen.start(0)')
-                sleep(3)
-                pps = []
-                for _ in range(5):
-                    pps += [ int(host.exec_pktgen('printf(pktgen.portStats("0", "rate")[0].pkts_rx)')) ]
-                    sleep(1)
-                host.exec_pktgen('pktgen.stop(0)')
-
-                print(f"Mean Mpps: {np.mean(pps)/1e6:.3f} (stddev: {np.std(pps)/1e6:.3f})")
                 # breakpoint()
                 # pass
                 # command = 'printf("Hello from Python!\\n")'
