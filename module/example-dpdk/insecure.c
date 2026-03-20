32e494315a0bd19450bbc2031c374543038e9558
Pogobanane <aenderboy@gmx.de>
2026-03-20 12:35:40 +0000

    insecure: reuse workload.h

--- module/example-dpdk/insecure.c
+++ module/example-dpdk/insecure.c
@@ -45,48 +45,7 @@
 #define NUM_MBUFS 2*512
 #define MBUF_CACHE_SIZE 250
 
-#ifndef WORKLOAD_STATE_SIZE_B
-#define WORKLOAD_STATE_SIZE_B 300 * 1024 * 1024
-#endif
-
-#ifndef WORKLOAD_ACCESSES_B
-#define WORKLOAD_ACCESSES_B 0
-#endif
-
-#if WORKLOAD_ACCESSES_B == 0 || WORKLOAD_STATE_SIZE_B == 0
-#define WORKLOAD_DISABLE
-#endif
-
-// --- workload (same as workload.h) ---
-
-struct workload {
-    uint64_t placeholder;
-    volatile uint64_t array[];
-};
-
-static struct workload* workload_alloc(void) {
-#ifndef WORKLOAD_DISABLE
-    struct workload* w = (struct workload*) malloc(sizeof(struct workload) + WORKLOAD_STATE_SIZE_B);
-    memset((void*)w, -1, sizeof(struct workload) + WORKLOAD_STATE_SIZE_B);
-    srand(12345);
-    return w;
-#else
-    return NULL;
-#endif
-}
-
-static inline void artificial_workload(struct workload* self, size_t nr_packets) {
-#ifndef WORKLOAD_DISABLE
-    const size_t array_len = WORKLOAD_STATE_SIZE_B / sizeof(uint64_t);
-    uint64_t r = rand();
-    for (size_t i = 0; i < WORKLOAD_ACCESSES_B * nr_packets / sizeof(uint64_t); i++) {
-        uint64_t pos = r % array_len;
-        uint64_t data = self->array[pos];
-        r = data ^ (r << 24 ^ r << 16 ^ r << 8 ^ r >> 16);
-    }
-    *(volatile uint64_t*)&self->placeholder = r;
-#endif
-}
+#include "workload.h"
 
 // --- timing ---
 
